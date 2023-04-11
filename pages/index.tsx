cbb8e0d9266af165bf7d782b71126d1b128286d5
Alberto Guerra Gonzalez <aguerra@riotgames.com>
2023-04-11 10:14:06 +0100

    feat: improve landing

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,25 +1,32 @@
 import * as React from 'react';
-import Image from 'next/image';
-import { Stack } from '@mui/material';
-import BasketImage from '@/public/assets/landing/basket.jpg';
-import { Brightness1 } from '@mui/icons-material';
+import Grid from '@mui/material/Unstable_Grid2';
+import { Button } from '@mui/material';
+import Router from 'next/router';
 
 export default function Landing() {
   return (
     <>
-      <Stack>
-        <Image
-          alt="Under development"
-          src={BasketImage}
-          style={{
-            maxHeight: 800,
-            width: 'auto',
-            opacity: 0.9,
-            filter: 'brightness(0.6)'
-          }}
-        />
-        <h1>My Tennis Coach </h1>
-      </Stack>
+      <Grid
+        container
+        sx={{
+          backgroundImage: 'url("/assets/landing/basket.jpg")',
+          backgroundRepeat: 'no-repeat',
+          backgroundSize: 'cover',
+          height: '800px',
+          width: '100%'
+        }}
+      >
+        <Grid xs={1} sx={{ height: '100%', width: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+          <Grid container spacing={2}>
+            <Grid xs={6}>
+            <Button variant="contained" onClick={() => Router.push('signin')}>Join as a Player</Button>
+            </Grid>
+            <Grid xs={6}>
+            <Button variant="contained" disabled>Join as a Coach</Button>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
     </>
   );
 }
