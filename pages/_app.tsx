e7a966f17be5a896e9242c91f3c241485572c486
Alberto Guerra Gonzalez <aguerra@riotgames.com>
2023-05-16 09:15:52 +0100

    feat: cookiless posthog

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,26 @@
 import '@/styles/globals.css';
-import type { AppProps } from 'next/app';
 import { SessionContextProvider } from '@supabase/auth-helpers-react';
 import { createBrowserSupabaseClient } from '@supabase/auth-helpers-nextjs';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { MyUserContextProvider } from '@/utils/useUser';
 import Layout from '@/layouts/layout';
 import { Database } from '@/lib/database.types';
+import posthog from 'posthog-js';
+import { PostHogProvider } from 'posthog-js/react';
+import { useRouter } from 'next/router';
+
+// Check that PostHog is client-side (used to handle Next.js SSR)
+if (typeof window !== 'undefined' && process.env.NEXT_PUBLIC_POSTHOG_KEY) {
+  console.log('Initializing PostHog');
+  posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
+    api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST || 'https://app.posthog.com',
+    persistence: 'memory',
+    // Enable debug mode in development
+    loaded: (posthog) => {
+      if (process.env.NODE_ENV === 'development') posthog.debug();
+    }
+  });
+}
 
 export default function App({ Component, pageProps }: any) {
   const [supabaseClient] = useState(() =>
@@ -18,13 +33,27 @@ export default function App({ Component, pageProps }: any) {
 
   const getLayout = Component.getLayout || getDefaultLayout;
 
+  const router = useRouter();
+
+  useEffect(() => {
+    // Track page views
+    const handleRouteChange = () => posthog?.capture('$pageview');
+    router.events.on('routeChangeComplete', handleRouteChange);
+
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, []);
+
   return (
     <>
-      <SessionContextProvider supabaseClient={supabaseClient}>
-        <MyUserContextProvider>
-          {getLayout(<Component {...pageProps} />)}
-        </MyUserContextProvider>
-      </SessionContextProvider>
+      <PostHogProvider client={posthog}>
+        <SessionContextProvider supabaseClient={supabaseClient}>
+          <MyUserContextProvider>
+            {getLayout(<Component {...pageProps} />)}
+          </MyUserContextProvider>
+        </SessionContextProvider>
+      </PostHogProvider>
     </>
   );
 }
