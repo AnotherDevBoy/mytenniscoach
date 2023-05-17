8e482721ecd0a675902d565221e9dd3e38053519
Alberto Guerra Gonzalez <aguerra@riotgames.com>
2023-05-17 13:24:44 +0100

    feat: spinner in dashboard

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,7 +1,13 @@
 import * as React from 'react';
 import { useUser } from '@supabase/auth-helpers-react';
 import Router from 'next/router';
-import { Card, CardContent, Typography } from '@mui/material';
+import {
+  Box,
+  Card,
+  CardContent,
+  CircularProgress,
+  Typography
+} from '@mui/material';
 import Grid from '@mui/material/Unstable_Grid2';
 import { StatsDTO } from '@/lib/types';
 import { getStats } from '@/lib/api';
@@ -18,39 +24,51 @@ const Dashboard = () => {
   }, [user]);
 
   React.useEffect(() => {
-    getStats().then((s) => setStats(s));
-  }, []);
+    if (user) {
+      getStats().then((s) => setStats(s));
+    }
+  }, [user]);
 
   if (user && stats) {
     return (
-      <>
-        <Grid container spacing={2}>
-          <Grid xs={12} md={6} lg={4} xl={2}>
-            <Card sx={{ height: 100 }}>
-              <CardContent>
-                <Typography variant="h5" component="div">
-                  Win-rate
-                </Typography>
-                <Typography variant="body2">{stats?.winRate}%</Typography>
-              </CardContent>
-            </Card>
-          </Grid>
-          <Grid xs={12} md={6} lg={4} xl={2}>
-            <Card sx={{ height: 100 }}>
-              <CardContent>
-                <Typography variant="h5" component="div">
-                  Nemesis
-                </Typography>
-                <Typography variant="body2">{stats.nemesis}</Typography>
-              </CardContent>
-            </Card>
-          </Grid>
+      <Grid container spacing={2}>
+        <Grid xs={12} md={6} lg={4} xl={2}>
+          <Card sx={{ height: 100 }}>
+            <CardContent>
+              <Typography variant="h5" component="div">
+                Win-rate
+              </Typography>
+              <Typography variant="body2">{stats?.winRate}%</Typography>
+            </CardContent>
+          </Card>
         </Grid>
-      </>
+        <Grid xs={12} md={6} lg={4} xl={2}>
+          <Card sx={{ height: 100 }}>
+            <CardContent>
+              <Typography variant="h5" component="div">
+                Nemesis
+              </Typography>
+              <Typography variant="body2">{stats.nemesis}</Typography>
+            </CardContent>
+          </Card>
+        </Grid>
+      </Grid>
     );
   }
 
-  return <></>;
+  return (
+    <Box
+      sx={{
+        width: '100%',
+        height: '90%', // TODO: Is there a better way?
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center'
+      }}
+    >
+      <CircularProgress />
+    </Box>
+  );
 };
 
 export default Dashboard;
