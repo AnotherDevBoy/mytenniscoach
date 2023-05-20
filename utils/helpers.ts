3e528690e9272f54eb14f032376e8e7ff21a5e22
Alberto Guerra Gonzalez <aguerra@riotgames.com>
2023-05-20 22:43:03 +0100

    fix: Google Auth

--- utils/helpers.ts
+++ utils/helpers.ts
@@ -1,9 +1,7 @@
 import { NextApiRequest } from 'next';
 
 export const getURL = () => {
-  let url =
-    process?.env?.NEXT_PUBLIC_SITE_URL ?? // Set this to your site URL in production env.
-    'http://localhost:3000/';
+  let url = 'https://mytenniscoach.io';
   // Make sure to include `https://` when not localhost.
   url = url.includes('http') ? url : `https://${url}`;
   // Make sure to including trailing `/`.
