71e0fc0a425512dedf04911bc5b65fae73c477bd
Alberto Guerra Gonzalez <aguerra@riotgames.com>
2023-05-28 19:50:59 +0100

    fix: more resilient stats

--- pages/api/stats/index.ts
+++ pages/api/stats/index.ts
@@ -18,54 +18,56 @@ export default async function handler(
 
   switch (req.method) {
     case 'GET':
-      const eventsDAL = await repository.getEvents(user);
+      try {
+        const matchesDAL = await repository.getCompletedMatches(user);
 
-      const completedMatches = eventsDAL.filter(
-        (e) => e.type === EventTypeDAL.Match && e.metadata
-      );
+        if (!matchesDAL || matchesDAL.length === 0) {
+          return res.status(200).json({
+            winRate: 100,
+            nemesis: 'N/A'
+          });
+        }
 
-      if (!completedMatches || completedMatches.length === 0) {
+        const matchResults = matchesDAL.map(
+          (e) => e.metadata as MatchEventData
+        );
+
+        const victories = matchResults.filter((r) => r.summary.win);
+
+        const lossesPerOpponent = new Map<string, number>();
+
+        matchesDAL
+          .filter((m) => {
+            const data = m.metadata as MatchEventData;
+            return !data.summary.win;
+          })
+          .forEach((m) => {
+            const losses = lossesPerOpponent.get(m.opponent_id!);
+            lossesPerOpponent.set(m.opponent_id!, losses ? losses + 1 : 1);
+          });
+
+        let opponent = undefined;
+
+        if (lossesPerOpponent.size > 0) {
+          const max = [...lossesPerOpponent.entries()].reduce(
+            (accumulator, element) => {
+              return element[1] > accumulator[1] ? element : accumulator;
+            }
+          );
+
+          opponent = await repository.getOpponent(user, max[0]);
+        }
+
+        return res.status(200).json({
+          winRate: ((victories.length / matchesDAL.length) * 100).toFixed(2),
+          nemesis: opponent ? opponent.name : 'N/A'
+        });
+      } catch (e) {
+        console.log('An error ocurred', e);
         return res.status(200).json({
           winRate: 100,
           nemesis: 'N/A'
         });
       }
-
-      const matchResults = completedMatches.map(
-        (e) => e.metadata as MatchEventData
-      );
-
-      const victories = matchResults.filter((r) => r.summary.win);
-
-      const lossesPerOpponent = new Map<string, number>();
-
-      completedMatches
-        .filter((m) => {
-          const data = m.metadata as MatchEventData;
-          return !data.summary.win;
-        })
-        .forEach((m) => {
-          const losses = lossesPerOpponent.get(m.opponent_id!);
-          lossesPerOpponent.set(m.opponent_id!, losses ? losses + 1 : 1);
-        });
-
-      let opponent = undefined;
-
-      if (lossesPerOpponent.size > 0) {
-        const max = [...lossesPerOpponent.entries()].reduce(
-          (accumulator, element) => {
-            return element[1] > accumulator[1] ? element : accumulator;
-          }
-        );
-
-        opponent = await repository.getOpponent(user, max[0]);
-      }
-
-      return res.status(200).json({
-        winRate: ((victories.length / completedMatches.length) * 100).toFixed(
-          2
-        ),
-        nemesis: opponent ? opponent.name : 'N/A'
-      });
   }
 }
